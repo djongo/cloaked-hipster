0290b9d4f5f03ba91d7a6bc87303e86f718857a2
Djongo <jonasholstein@gmail.com>
2013-09-15 09:23:47 +0200

    Add mailer and layout to master data tables, adjusting controllers as well

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -12,25 +12,25 @@ class EmailsController < ApplicationController
 
   # GET /emails/1
   # GET /emails/1.json
-  def show
-    @email = Email.find(params[:id])
+  # def show
+  #   @email = Email.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @email }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.json { render json: @email }
+  #   end
+  # end
 
   # GET /emails/new
   # GET /emails/new.json
-  def new
-    @email = Email.new
+  # def new
+  #   @email = Email.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @email }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.json { render json: @email }
+  #   end
+  # end
 
   # GET /emails/1/edit
   def edit
@@ -39,19 +39,19 @@ class EmailsController < ApplicationController
 
   # POST /emails
   # POST /emails.json
-  def create
-    @email = Email.new(params[:email])
+  # def create
+  #   @email = Email.new(params[:email])
 
-    respond_to do |format|
-      if @email.save
-        format.html { redirect_to @email, notice: 'Email was successfully created.' }
-        format.json { render json: @email, status: :created, location: @email }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @email.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @email.save
+  #       format.html { redirect_to @email, notice: 'Email was successfully created.' }
+  #       format.json { render json: @email, status: :created, location: @email }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @email.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PUT /emails/1
   # PUT /emails/1.json
@@ -71,13 +71,13 @@ class EmailsController < ApplicationController
 
   # DELETE /emails/1
   # DELETE /emails/1.json
-  def destroy
-    @email = Email.find(params[:id])
-    @email.destroy
+  # def destroy
+  #   @email = Email.find(params[:id])
+  #   @email.destroy
 
-    respond_to do |format|
-      format.html { redirect_to emails_url }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to emails_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,48 +10,10 @@ class PagesController < ApplicationController
     end
   end
 
-  # GET /pages/1
-  # GET /pages/1.json
-  # def show
-  #   @page = Page.find(params[:id])
-
-  #   respond_to do |format|
-  #     format.html # show.html.erb
-  #     format.json { render json: @page }
-  #   end
-  # end
-
-  # GET /pages/new
-  # GET /pages/new.json
-  # def new
-  #   @page = Page.new
-
-  #   respond_to do |format|
-  #     format.html # new.html.erb
-  #     format.json { render json: @page }
-  #   end
-  # end
-
   # GET /pages/1/edit
   def edit
     @page = Page.find(params[:id])
   end
-
-  # POST /pages
-  # POST /pages.json
-  # def create
-  #   @page = Page.new(params[:page])
-
-  #   respond_to do |format|
-  #     if @page.save
-  #       format.html { redirect_to @page, notice: 'Page was successfully created.' }
-  #       format.json { render json: @page, status: :created, location: @page }
-  #     else
-  #       format.html { render action: "new" }
-  #       format.json { render json: @page.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
 
   # PUT /pages/1
   # PUT /pages/1.json
@@ -68,18 +30,6 @@ class PagesController < ApplicationController
       end
     end
   end
-
-  # DELETE /pages/1
-  # DELETE /pages/1.json
-  # def destroy
-  #   @page = Page.find(params[:id])
-  #   @page.destroy
-
-  #   respond_to do |format|
-  #     format.html { redirect_to pages_url }
-  #     format.json { head :no_content }
-  #   end
-  # end
 
   def home
   	@title = "Home"
