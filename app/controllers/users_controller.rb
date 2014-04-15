6ae7c588b58a0c5a727d46a0ab280dab91f1c851
Djongo <jonasholstein@gmail.com>
2014-04-15 20:38:48 +0200

    Change size on author dropdowns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,4 +48,11 @@ class UsersController < ApplicationController
       render :action => 'edit'
     end
   end  
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url, notice: "User removed."
+  end
+
 end
