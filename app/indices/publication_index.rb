90bd7125900dba201bdd05cc1152b7d0a50d4414
Djongo <jonasholstein@gmail.com>
2015-02-18 19:55:16 +0100

    Change to new syntax in thinking sphinx index

--- app/indices/publication_index.rb
+++ app/indices/publication_index.rb
@@ -7,19 +7,19 @@ ThinkingSphinx::Index.define :publication, :with => :active_record do
     indexes state, :sortable => true
     indexes reference
     indexes target_journal.name, :as => :target_journal_name
-    indexes populations(:name), :as => :inclusion_name
-    indexes surveys(:name), :as => :foundation_name
+    indexes populations.name, :as => :inclusion_name
+    indexes surveys.name, :as => :foundation_name
     # indexes notes.content, :as => :note_content
     
     # Keywords, mediators, outcomes, and determinants
     # indexes keyword_variables(:name), :as => :keyword_name
-    indexes variables(:name), :as => :variable_name
+    indexes variables.name, :as => :variable_name
 
     # People information
     indexes [user.first_name, user.last_name, user.email], :as => :responsible_author
 
     # Author information
-    indexes authors(:name), :as => :author_name
+    indexes authors.name, :as => :author_name
     indexes authors.email, :as => :author_email
     indexes authors.country_team.name, :as => :country_team_name
     indexes authors.focus_group.name, :as => :focus_group_name
